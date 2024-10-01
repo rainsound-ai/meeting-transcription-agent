0676abd1091052da7d6cc32c724346d5613db85c
alexandereversbusch <abusch419@gmail.com>
2024-10-01 13:40:34 -0400

    Edit shared urls and render.yaml.

--- frontend/src/lib/Urls.shared.ts
+++ frontend/src/lib/Urls.shared.ts
@@ -1,12 +1,11 @@
 import { dev } from "$app/environment";
 
-export function root(): string {
-  return "/";
-}
-
 export function apiRoot(): string {
   if (dev) {
+    // Development environment (localhost)
     return "http://localhost:8000/api";
   }
-  return "/api";
+  
+  // Production environment (use an environment variable or hardcoded URL)
+  return import.meta.env.VITE_API_BASE_URL
 }
